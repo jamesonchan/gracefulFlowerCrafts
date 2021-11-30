63327fb71cd55089bfd907915fe4597060f67bbe
Jameson Chen <[jamesonychan@gmail.com]>
2021-11-30 04:05:52 -0500

    added NProgress functionality to display progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,27 @@
-import 'tailwindcss/tailwind.css'
-import '../styles/globals.css'
-import {SessionProvider} from 'next-auth/react'
-import {CartContextProvider} from '../contexts/CartContextProvider'
+import "tailwindcss/tailwind.css";
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import { CartContextProvider } from "../contexts/CartContextProvider";
+import NextNProgress from "nextjs-progressbar";
 
-
-
-
-function MyApp({ Component, pageProps:{session,...pageProps} }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
+    <>
+      <NextNProgress
+        color="#f59e0b"
+        startPosition={0.2}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+        options={{showSpinner:false}}
+      />
+      <SessionProvider session={session}>
         <CartContextProvider>
           <Component {...pageProps} />
         </CartContextProvider>
-        
-    </SessionProvider>
-
-  )
+      </SessionProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
